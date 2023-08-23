c77611a52e1fde7588c1715b6ef3209cef55557f
oahmed105 <oahmed10598@gmail.com>
2023-08-23 14:45:55 -0400

    all calculator tests passed

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,35 +1,47 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        ArrayList<Double> newArr = new ArrayList<>();
+        for (int i = 0; i < value.length; i++){
+            newArr.add(squareRoot(value[i]));
+        }
+        Double[] doublesArr = newArr.toArray(new Double[0]);
+        return doublesArr;
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        ArrayList<Double> newArr = new ArrayList<>();
+        for (int i = 0; i < values.length; i++){
+            newArr.add(square(values[i]));
+        }
+        Double[] doublesArr = newArr.toArray(new Double[0]);
+        return doublesArr;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor / dividend;
     }
 }
