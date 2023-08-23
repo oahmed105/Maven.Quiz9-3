b2e352313bb067108a1213eb1288445d978faf25
oahmed105 <oahmed10598@gmail.com>
2023-08-23 16:38:05 -0400

    most tests passed

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -27,7 +27,21 @@ public class StringUtils {
 
     /* isogram (plural isograms) A word or phrase in which each letter occurs the same number of times. read the tests. */
     public static Boolean isIsogram(String str) {
-        return null;
+        for (int i = 0; i < str.length(); i++) {
+
+            // Counting occurrences of s[i]
+            int count = 1;
+            while (i + 1 < str.length()
+                    && str.charAt(i)
+                    == str.charAt(i + 1)) {
+                i++;
+                count++;
+            }
+            if (count == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
@@ -40,10 +54,22 @@ public class StringUtils {
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
+//        String newStr = "";
+//        for (int i = 0; i < str.length(); i++){
+//            char charAtPosition = str.charAt(i);
+//            if (charAtPosition)
+//        }
         return null;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++)
+        {
+            chars[i] = Character.isUpperCase(chars[i])
+                    ? Character.toLowerCase(chars[i])
+                    : Character.toUpperCase(chars[i]);
+        }
+        return new String(chars);
     }
 }
