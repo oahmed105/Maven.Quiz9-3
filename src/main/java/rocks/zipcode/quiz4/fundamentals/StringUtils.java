f2a4c7cdd13248d8a66c7ebd8accea219462af14
oahmed105 <oahmed10598@gmail.com>
2023-08-23 15:41:41 -0400

    making progress on StringUtils

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,19 +1,28 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(str.length()/2, Character.toUpperCase(getMiddleCharacter(str)));
+
+        return sb.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(str.length()/2, Character.toLowerCase(getMiddleCharacter(str)));
+
+        return sb.toString();
     }
 
     /* isogram (plural isograms) A word or phrase in which each letter occurs the same number of times. read the tests. */
@@ -22,7 +31,12 @@ public class StringUtils {
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for (int i = 0; i < str.length() - 1; i++){
+            if (str.charAt(i) == str.charAt(i + 1)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
